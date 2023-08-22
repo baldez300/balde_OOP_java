d2271f8fe62ff4315cbc9622a0c35caf8a84c009
Balde641 <62099893+Balde641@users.noreply.github.com>
2023-08-22 13:25:03 +0300

    Task 1 done

--- src/Hello.java
+++ src/Hello.java
@@ -1,2 +1,7 @@
+// A simple program that greets the user with a fixed message.
+
 public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello Students!");
+    }
 }
