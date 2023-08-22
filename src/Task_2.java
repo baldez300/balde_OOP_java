06d6aae96a3724847edcadc2c0b393a37fb4c020
Balde641 <62099893+Balde641@users.noreply.github.com>
2023-08-22 13:37:59 +0300

    Task 2 done

---
+++ src/Task_2.java
@@ -0,0 +1,15 @@
+// A program that prints the pyramid with asterisks(*)
+//    *
+//   ***
+//  *****
+// *******
+//
+
+public class Task_2 {
+    public static void main(String[] args) {
+        System.out.println("   *");
+        System.out.println("  ***");
+        System.out.println(" *****");
+        System.out.println("*******");
+    }
+}
