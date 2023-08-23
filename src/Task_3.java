d18e9bbf7a7dc95082f5d65230c7a79fb494d6bb
Balde641 <62099893+Balde641@users.noreply.github.com>
2023-08-23 12:54:59 +0300

    Task 3 done

--- src/Task_3.java
+++ src/Task_3.java
@@ -12,13 +12,13 @@ public class Task_3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Give the first number:");
+        System.out.print("Give the first number: ");
         int first = Integer.parseInt(scanner.nextLine());
 
-        System.out.println("Give the second number:");
+        System.out.print("Give the second number: ");
         int second = Integer.parseInt(scanner.nextLine());
 
-        System.out.println("Give the third number:");
+        System.out.print("Give the third number: ");
         int third = Integer.parseInt(scanner.nextLine());
 
         double average = first + second + third;
